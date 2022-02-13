c3d738ca57cd55a2de2710840c8babaafe53166e
Amen Olabode <olabodeamen4@gmail.com>
2022-02-13 21:57:52 +0000

    updated welcome texts

--- lib/screens/home_page_3.dart
+++ lib/screens/home_page_3.dart
@@ -8,10 +8,10 @@ class HomePage3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Template(
       image: AssetImage('images/fitness3.jpg'),
-      title: 'Welcome!',
-      text1: 'Explore the all new way to build',
-      text2: 'your fitness & find the',
-      text3: 'perfect fit for you',
+      title: "Let's Go!",
+      text1: 'Get the new fitness upadates',
+      text2: 'everyday, and improve',
+      text3: 'performance',
       iconImage: AssetImage('images/Breadcrumb1.png'),
       navigateTo: "",
       buttonAction: "/firstpage",
