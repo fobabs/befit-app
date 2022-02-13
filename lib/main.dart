41c4171b5288a8afe560c6d41ca1519aff60cebc
FOBABS <fobabs2win@rocketmail.com>
2022-02-13 20:15:52 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:befit_app/screens/home_page_1.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: HomePage1(),
     );
   }
 }
