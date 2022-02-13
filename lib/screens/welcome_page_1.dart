02224cf9e52e10e417c697b3a171f629c7f45ddd
FOBABS <fobabs2win@rocketmail.com>
2022-02-13 23:07:22 +0000

    Rename homePage to welcomePage

--- lib/screens/welcome_page_1.dart
+++ lib/screens/welcome_page_1.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:befit_app/template/template.dart';
 
-class HomePage1 extends StatelessWidget {
-  const HomePage1({Key? key}) : super(key: key);
+class WelcomePage1 extends StatelessWidget {
+  const WelcomePage1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Template(
-      image: AssetImage('images/fitness1.jpg'),
+      image: AssetImage('images/Homescreen1.png'),
       title: 'Welcome!',
       text1: 'Explore the all new way to build',
       text2: 'your fitness & find the',
