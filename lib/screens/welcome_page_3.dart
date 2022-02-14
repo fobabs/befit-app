fb8b4a1c5801e3b6d0b15d17b708142bbc075870
Poga Kuofie <mail@pogakuofie.com>
2022-02-14 11:03:59 +0000

    feat: developed the third onbosrding screen

--- lib/screens/welcome_page_3.dart
+++ lib/screens/welcome_page_3.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:befit_app/template/template.dart';
 
-class WelcomePage2 extends StatelessWidget {
-  const WelcomePage2({Key? key}) : super(key: key);
+class WelcomePage3 extends StatelessWidget {
+  const WelcomePage3({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Template(
       image: AssetImage('images/Homescreen2.png'),
       title: 'Customize',
-      text1: 'Feel free to adjust your workout',
-      text2: '& diet settings according to',
-      text3: 'your comfort',
+      text1: 'This is screen three',
+      text2: '& created by github.com/pogakuofie',
+      text3: 'dont mind the image.',
       iconImage: AssetImage('images/Breadcrumb2.png'),
-      buttonAction: '/screen1',
+      buttonAction: '/screen3',
     );
   }
 }
